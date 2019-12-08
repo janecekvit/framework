955d192e53b86f30c82de61b9086000fffff4cf2
Vít Janeček <janecekvit@outlook.com>
2019-12-08 21:33:22 +0100

    On Mitm: Move some classes to the Extensions

---
+++ Extensions/Finally.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <functional>
+
+/// <summary>
+/// CFinally class implements finally block in C++
+/// </summary>
+class CFinally
+{
+public:
+	CFinally(_In_ std::function<void(void)> &&fnCallback)
+		: m_fnCallback(std::move(fnCallback))
+	{
+	}
+
+	virtual ~CFinally()
+	{
+		try
+		{
+			m_fnCallback();
+		}
+		catch (...) {} //check double exception serious error
+		
+	}
+
+private:
+	std::function<void(void)> m_fnCallback = nullptr;
+};
