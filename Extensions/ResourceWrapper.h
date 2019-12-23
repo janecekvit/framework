e788f1ed6945f45723ceb7f0884130dea91f9129
Vít Janeček <janecekvit@outlook.com>
2019-12-24 00:01:45 +0100

    Create UT to cover ResourceWrapper functionality and reflect changes from it to the ResourceWrapper

--- Extensions/ResourceWrapper.h
+++ Extensions/ResourceWrapper.h
@@ -15,7 +15,20 @@ Purpose:	header file contains RAII pattern
 #include <functional>
 #include "Framework/Extensions/GetterSetter.h"
 
-
+/// <summary>
+/// The wrapper implemented own deleter that gains functionality to release all used resources correctly.
+/// Method derives from the GetterSetter class to using input resource with implicit conversions.
+/// Ensure that deleter's body can be called multiple times to handle deallocations of the same resource in case of CopyConstructible methods of this wrapper are used.
+/// </summary>
+/// <example>
+/// <code>
+/// auto oWrapperFile = ResourceWrapper<std::fstream>(std::fstream("Test", std::ios::binary), [](std::fstream& i)
+/// {
+///		i.close();
+/// });
+///	bool open = oWrapperFile->is_open();
+/// </code>
+/// </example>
 template <class TResource>
 class ResourceWrapper 
 	: public GetterSetter<TResource>
@@ -54,10 +67,43 @@ public:
 		} 
 	}
 
-	ResourceWrapper(const ResourceWrapper&) = default;
-	ResourceWrapper& operator=(const ResourceWrapper&) = default;
-	ResourceWrapper(ResourceWrapper&&) = default;
-	ResourceWrapper& operator=(ResourceWrapper&&) = default;
+	ResourceWrapper(const ResourceWrapper& oOther)
+	{
+		ResourceWrapper::operator=(oOther);
+	}
+
+	ResourceWrapper(ResourceWrapper&& oOther)
+	{
+		ResourceWrapper::operator=(std::move(oOther));
+	}
+
+	ResourceWrapper& operator=(const ResourceWrapper& oOther)
+	{
+		ResourceWrapper::operator=(oOther.m_oResource);
+		m_fnDeleter = oOther.m_fnDeleter;
+		return *this;
+	}
+
+	ResourceWrapper& operator=(ResourceWrapper&& oOther)
+	{
+		ResourceWrapper::operator=(std::move(oOther.m_oResource));
+		m_fnDeleter = std::move(oOther.m_fnDeleter);
+		return *this;
+	}
+
+	ResourceWrapper& operator=(const TResource& oResource)
+	{
+		Reset();
+		GetterSetter<TResource>::m_oResource = oResource;
+		return *this;
+	}
+
+	ResourceWrapper& operator=(TResource&& oResource)
+	{
+		Reset();
+		GetterSetter<TResource>::m_oResource = std::move(oResource);
+		return *this;
+	}
 
 	void Reset()
 	{
